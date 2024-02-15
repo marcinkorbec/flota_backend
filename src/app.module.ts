b164fd0e045883e86256d81b9ff56936adba887d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-02-15 22:04:21 +0100

    The application's listening port has been changed to 3001 and the TypeORM module has been added to the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'nwb',
+    password: 'nwb',
+    database: 'nwb-flota',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
